f70504e517cb4b20f89e6c0a06367b84ddd9360a
Michael Nikitochkin <miry.sof@gmail.com>
2014-09-18 21:52:19 +0200

    :panda_face: Added action api to welcome.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
+
+  doorkeeper_for :api
+
+  respond_to :json, :html
+
   def index
+  end
+
+  def api
+    respond_with current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get 'api' => 'welcome#api'
 
   devise_for :users
   use_doorkeeper
